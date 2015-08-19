bb8a60d726c8193b2f77acc8081d93ccaae465a1
pkumar38 <pkumar38@gmail.com>
2015-08-19 17:22:39 +0800

    Update xio_snap_args.pl

--- xio_snap_args.pl
+++ xio_snap_args.pl
@@ -6,7 +6,7 @@
 # will also map the created snapshots into $BACKUP host, where the backup are taken from these 
 # volumes. 
 # The argument this script takes is DB-DATA or DB-LOGS. Default is DB in case no argument is provided. 
-# Requirements. 
+# Requirements 
 # Create two consistency groups based on database data luns and database archive logs.
 # Consistency group for database dataluns group as 'DB-DATA'
 # Consistency group for database archive logs group as 'DB-LOGS'
@@ -17,7 +17,10 @@
 # 13082015 2.0 New requirement - Initiating the scripts in sequence for different set of Snap-hosts.
 # 13082015 2.1 Get argument and based on the value, take the snapshots accordingly. 
 # 14082015 2.1 Unit testing done. All tests passed.  
-# 
+
+# LWP module normally tries to verify the hostname based on CA cert. 
+# we dont want this and let us by-pass this.
+$ENV{'PERL_LWP_SSL_VERIFY_HOSTNAME'} = 0;
 
 #Of course all perl programs start like this !
 use strict;
@@ -30,28 +33,44 @@ use warnings;
 #                        $now[5]+1900, $now[4]+1, $now[3],
 #                        $now[2],      $now[1],   $now[0]);
                         
-my $_WORKGRP = shift || 'DB-DATA';
-$_WORKGRP =~ tr/a-z/A-Z/;
-my $FROMCONSID = $_WORKGRP;
-my $TOCONSID = "SnapshotSet." . $_WORKGRP;
-my $SNAPSUFFIX = ".snapshot." . $_WORKGRP;
-my $BACKUPIG = "Host1";
-
 my $verbose=0;
 
-my $username="admin";
-my $password="Xtrem10";
-my $xms="sgrdcxio";
+#my $username="admin";
+#my $password="Xtrem10";
+#my $xms="172.20.166.29";
+
+my $username;
+my $password;
+my $xms;
 
 # To make Perl find the modules without the package being installed. 
-use lib '../../lib', '.';
-
-# These are your friends. 
+# They are your friends. 
 use REST::Client;
 use MIME::Base64;
 use JSON;
 use Data::Dumper;
+use XML::Simple;
+use Data::Dumper;
 #use Try::Tiny;
+
+if (! -e "/home/xtuser/storinfra/emc/data/"){
+   print "The directory doesn't exsit. /home/xtuser/storinfra/emc/data/";
+}
+my $file="/home/xtuser/storinfra/emc/data/properties.xml";
+if (open my $fh, '<', $file){
+        my $snap_needs = XMLin('/home/xtuser/storinfra/emc/data/properties.xml');
+        #my $user, $key, $machine;
+        ##print Dumper($snap_needs);
+
+        $username = $snap_needs->{user}->{name};
+        $password = $snap_needs->{user}->{key};
+        $xms = $snap_needs->{user}->{machine};
+
+        #print "$user \n$key \n$machine \n";
+} else {
+        #print "File not available - /home/xtuser/storinfra/emc/data/properties.xml";
+        write_to_err ("XIO_SNAP.PL Err - File not available - /home/xtuser/storinfra/emc/data/properties.xml.") or die qq (Can't write to the Err file);
+}
 
 ##split and join
 #    @mydata = ( "Simpson:Homer:1-800-000-0000:40:M",
@@ -64,6 +83,37 @@ use Data::Dumper;
 #        print "You may call $age year old $first $last at $phone.\n";
 #    }
 
+## Write every log into log file, when i'm called. 
+sub write_to_log {
+	my @now = localtime();
+	my $daystamp = sprintf("%04d%02d%02d", 
+                        $now[5]+1900, $now[4]+1, $now[3]);
+        my $timestamp = sprintf("%04d%02d%02d%02d%02d%02d", 
+                        $now[5]+1900, $now[4]+1, $now[3],
+                        $now[2],      $now[1],   $now[0]);
+	my $filename = "/home/xtuser/storinfra/emc/logs/report_$daystamp.log";
+	my $message = shift;
+	open(my $fh, '>>', $filename) or die "Could not open file '$filename' $!";
+	return print {$fh} "$timestamp: $message\n";
+	close $fh;
+# print "done\n";
+}
+
+## Write every error into err log, when i'm called. 
+sub write_to_err {
+	my @now = localtime();
+	my $daystamp = sprintf("%04d%02d%02d", 
+                        $now[5]+1900, $now[4]+1, $now[3]);
+        my $timestamp = sprintf("%04d%02d%02d%02d%02d%02d", 
+                        $now[5]+1900, $now[4]+1, $now[3],
+                        $now[2],      $now[1],   $now[0]);
+	my $filename = "/home/xtuser/storinfra/emc/err/err_$daystamp.log";
+	my $message = shift;
+	open(my $fh, '>>', $filename) or die "Could not open file '$filename' $!";
+	return print {$fh} "$timestamp: $message\n";
+	close $fh;
+# print "done\n";
+}
 
 #
 # Check the response from all API calls, and exit if they fail
@@ -76,9 +126,27 @@ sub checkerr($) {
 	return 0 if ($respcode>=200 && $respcode <300);
 
 	my $msg = from_json($cl->responseContent())->{message};
-	print STDERR "Error - $msg (response code $respcode)\n";
+	#print STDERR "Error - $msg (response code $respcode)\n";
+        write_to_err ("XIO_SNAP.PL Err - $msg (response code $respcode)") or die qq (Can't write to the Err file);
 	exit(2);
 }
+
+if (@ARGV >1 || @ARGV < 1) {
+	# Error Codition Met. Write to Err Log.
+	write_to_err ("Error Condition. Err - Only one argument required !!") or die qq (Can't write to the Err file);
+        exit (2);
+}else{
+	# Success Condition. Write to Log file.
+	write_to_log ("XIO_SNAP.PL - Successful start of program.") or die qq (Can't write to the log file);
+}
+
+#my $_WORKGRP = shift || 'FM801DB-DATA';
+my $_WORKGRP = shift;
+$_WORKGRP =~ tr/a-z/A-Z/;
+my $FROMCONSID = $_WORKGRP;
+my $TOCONSID = "SnapshotSet." . $_WORKGRP;
+my $SNAPSUFFIX = ".snapshot." . $_WORKGRP;
+my $BACKUPIG = "LINUX_boise80_IG";
 
 my $client = REST::Client->new();
 my $headers = {Authorization => "Basic ".encode_base64($username.":".$password), "Content-Type" => 'application/json'};
@@ -116,16 +184,19 @@ if (keys (%$snmapping) > 0 ) {
 		foreach ( @val ) { 
 			($ig, $ll, $mapname) = split ( /:/ ); 
 			# Unmap the given snapshot volume
+		        # print "deleting the map name $mapname";
 			$client->DELETE("https://$xms/api/json/v2/types/lun-maps?name=$mapname", $headers);
 			checkerr($client);
 			}
 		# Delete the given volume - Change in NAA - Please note that. 
+                #print "Delete the volumes $ki";
 		$client->DELETE("https://$xms/api/json/v2/types/volumes/?name=$ki", $headers);
 		checkerr($client);
 	}
 } else {
-	# Gives me 0 - I couldn't find no earlier mappings. 
-	print "We don't have any snapshots to remove the mappings \n";
+	# Gives me 1 - I couldn't find no earlier mappings. 
+	#print "We don't have any snapshots to remove the mappings \n";
+        write_to_log ("XIO_SNAP.PL $FROMCONSID - We don't have any snapshots to remove the mappings.") or die qq (Can't write to the log file);
 }
 
 ## Create new snapshots now.
@@ -167,4 +238,6 @@ my @links = @{ $resp->{'links'} };
 		$resp = from_json($client->responseContent());
 	}
 # Finally, I'm done. Happy !
+write_to_log ("XIO_SNAP.PL $FROMCONSID - Successful end of program.") or die qq (Can't write to the log file);
 exit (0);
+
